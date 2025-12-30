7c0327e0715b40b33db5f6b0e470a7e8f7239289
asavy79 <asavard@uoguelph.ca>
2025-12-29 18:10:19 -0800

    set up initial migration scripts

---
+++ migrations/versions/685f6981c576_migrate_to_many_to_many_university_.py
@@ -0,0 +1,24 @@
+"""Migrate to many-to-many university relationships and update schemas
+
+Revision ID: 685f6981c576
+Revises: 
+Create Date: 2025-12-29 18:09:52.280272
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '685f6981c576'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
